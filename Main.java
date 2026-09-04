c2a4200aa4a7392d08b0171a0edd594e9753ecea
Shashvat Gupta <Shashvatgupta11@gmail.com>
2026-09-04 18:33:00 +0530

    Updated Greeting Message

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello buddy!");
+        System.out.println("Hello from my first GitHub project!");
     }
 }
